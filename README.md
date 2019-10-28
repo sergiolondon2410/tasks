8a25b81c31cc0b4f128a1a46b1d15d1be7a21a0b
sergiolondon2410 <sjlondono@opti.local>
2019-10-28 15:40:38 -0500

    first front-end settings

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tasks
 Task managers developed with mongodb, express and vuejs
+
+Run mongo
+mongod
+
+Run node server
+npm run dev
---
+++ src/app/components/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-light">
+            <a href="/" class="navbar-brand">MEVN Stack</a>
+        </nav>
+        <div class="container">
+            <div class="row pt-5">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <form @submit.prevent="addTask">
+                                <div class="form-group">
+                                    <input type="text" v-model="task.title" placeholder="Insert a new task" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <textarea cols="30" rows="10" v-model="task.description" placeholder="Insert a description" class="form-control"></textarea>
+                                </div>
+                                <button class="btn btn-primary btn-block">Send</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    // class Task {
+    //     constructor(title, description) {
+    //         this.title = title;
+    //         this.description = description;
+    //     }
+    // }
+    export default {
+        data() {
+            return {
+                task: {
+                    title: '',
+                    description: ''
+                }
+            }
+        },
+        methods: {
+            addTask() {
+                console.log(this.task);
+                this.clearTask();
+            },
+            clearTask() {
+                this.task.title = '';
+                this.task.description = '';
+            }
+        }
+    }
+</script>
